dcdf8118d7d4e55eaecbb433de6b26988c5f041b
unknown <omersim15185@gmail.com>
2023-12-19 21:18:17 +0200

    Add api to Weather Forecast

--- weather.js
+++ weather.js
@@ -1,49 +1,51 @@
-const apiKey="1e5a813a2afbafdcfa4de23fe1badf37";
-const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-export var city="gedera";
+const apiKey = "1e5a813a2afbafdcfa4de23fe1badf37";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+// import {zonda} from './weatherAllWeek'
 
 
 const searchBox = document.querySelector(".search input");
 const searchButton = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
-async function checkWeather(city){
-    const respose = await fetch(apiUrl + city +`&appid=${apiKey}`);
+async function checkWeather(city) {
+    const respose = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await respose.json();
 
     console.log(data);
 
-    document.querySelector(".city").innerHTML=data.name;
-    document.querySelector(".temp").innerHTML=data.main.temp;
-    document.querySelector(".humidity").innerHTML=data.main.humidity;
-    document.querySelector(".wind").innerHTML=data.wind.speed;
-    
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = data.main.temp;
+    document.querySelector(".humidity").innerHTML = data.main.humidity;
+    document.querySelector(".wind").innerHTML = data.wind.speed;
+
     if (data.weather[0].main == "Clouds") {
-        weatherIcon.src ="./images/clouds.png";
+        weatherIcon.src = "./images/clouds.png";
     }
-    else if(data.weather[0].main=="Rain"){
+    else if (data.weather[0].main == "Rain") {
         weatherIcon.src = "./images/rain.png";
     }
-    else if(data.weather[0].main=="Mist"){
+    else if (data.weather[0].main == "Mist") {
         weatherIcon.src = "./images/mist";
     }
-    else if(data.weather[0].main=="Snow"){
+    else if (data.weather[0].main == "Snow") {
         weatherIcon.src = "./images/snow";
     }
-    else if(data.weather[0].main=="Drizzle"){
+    else if (data.weather[0].main == "Drizzle") {
         weatherIcon.src = "./images/drizzle";
     }
-    else if(data.weather[0].main=="Clear"){
+    else if (data.weather[0].main == "Clear") {
         weatherIcon.src = "./images/clear.png";
     }
 
 
 }
 
+
+
 searchButton.addEventListener("click",()=>{
     checkWeather(searchBox.value);
     var city=searchBox.value;
     console.log(city);
-    
+    localStorage.setItem('shareData',searchBox.value);
 })
-
--- weatherAllWeek.js
+++ weatherAllWeek.js
@@ -1,20 +1,29 @@
+
 const apiKeyAllWeek="a10540913c3748439b4132120231512";
 const  apiUrlAllWeek="https://api.weatherapi.com/v1/forecast.json?&days=7&q=";
+const retrievedData = localStorage.getItem('shareData');
 
-import { city } from "../../Omer Projects/Weather App/weather";
-var cityNew=city;
-console.log(cityNew);
-// async function checkWeather(city){
-//     const respose = await fetch(apiUrlAllWeek + city +`&key=${apiKeyAllWeek}`);
-//     var dataAllWeek = await respose.json();
+function timedRefresh(timeoutPeriod) {
+	setTimeout("location.reload(true);",timeoutPeriod);
+}
 
-//     console.log(dataAllWeek);
+window.onload = timedRefresh(5000);
+console.log(retrievedData);
 
-//     // document.querySelector(".city").innerHTML=data.name;
-//     // document.querySelector(".temp").innerHTML=data.main.temp;
-//     // document.querySelector(".humidity").innerHTML=data.main.humidity;
-//     // document.querySelector(".wind").innerHTML=data.wind.speed;
-    
-    
 
-// }
+async function checkWeather(retrievedData){
+    const respose = await fetch(apiUrlAllWeek + retrievedData +`&key=${apiKeyAllWeek}`);
+    var dataAllWeek = await respose.json();
+
+    console.log(dataAllWeek);
+
+    // document.querySelector(".city").innerHTML = dataAllWeek.name;
+    // document.querySelector(".temp").innerHTML = dataAllWeek.main.temp;
+    // document.querySelector(".humidity").innerHTML = dataAllWeek.main.humidity;
+    // document.querySelector(".wind").innerHTML = dataAllWeek.wind.speed;
+
+}
+
+checkWeather(retrievedData);
+
+
