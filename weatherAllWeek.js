ea20d0dea6cdc4ea4c5d3c0075c0104f4db3f8ea
unknown <omersim15185@gmail.com>
2023-12-23 17:39:50 +0200

    Correcting the problem of adjusting the temp for the day

--- weatherAllWeek.js
+++ weatherAllWeek.js
@@ -21,59 +21,57 @@ async function checkWeather(city) {
     var dataAllWeek = await respose.json();
 
     console.log(dataAllWeek);
-    console.log(dataAllWeek.forecast.forecastday[0].day.condition.icon)
+    
     for (let i = 0; i < days.length; i++) {
         var d = new Date(dataAllWeek.forecast.forecastday[i].date);
         let day = d.getDay();
+        console.log(day)
 
 
         switch (day) {
             case 0:
-                document.querySelector(".tempSunday").innerHTML = dataAllWeek.forecast.forecastday[0].day.avgtemp_c;
-                weatherIconsWeek[0].src = `${dataAllWeek.forecast.forecastday[0].day.condition.icon}`
-                console.log(dataAllWeek.forecast.forecastday[0].day.avgtemp_c, day)
+                document.querySelector(".tempSunday").innerHTML = dataAllWeek.forecast.forecastday[i].day.avgtemp_c;
+                weatherIconsWeek[0].src = `${dataAllWeek.forecast.forecastday[i].day.condition.icon}`
+                console.log(dataAllWeek.forecast.forecastday[i].day.condition.icon)
                 break;
             case 1:
-                document.querySelector(".tempMonday").innerHTML = dataAllWeek.forecast.forecastday[1].day.avgtemp_c;
-                weatherIconsWeek[1].src = `${dataAllWeek.forecast.forecastday[1].day.condition.icon}`
-                console.log(dataAllWeek.forecast.forecastday[1].day.avgtemp_c, day)
-                console.log(dataAllWeek.forecast.forecastday[1].day.avgtemp_c, day)
+                document.querySelector(".tempMonday").innerHTML = dataAllWeek.forecast.forecastday[i].day.avgtemp_c;
+                weatherIconsWeek[1].src = `${dataAllWeek.forecast.forecastday[i].day.condition.icon}`
+                console.log(dataAllWeek.forecast.forecastday[i].day.avgtemp_c, day)
+               
                 break;
 
             case 2:
-                document.querySelector(".tempTuesday").innerHTML = dataAllWeek.forecast.forecastday[2].day.avgtemp_c;
-                weatherIconsWeek[2].src = `${dataAllWeek.forecast.forecastday[2].day.condition.icon}`
-                console.log(dataAllWeek.forecast.forecastday[2].day.avgtemp_c, day)
-                console.log(dataAllWeek.forecast.forecastday[2].day.avgtemp_c, day)
+                document.querySelector(".tempTuesday").innerHTML = dataAllWeek.forecast.forecastday[i].day.avgtemp_c;
+                weatherIconsWeek[2].src = `${dataAllWeek.forecast.forecastday[i].day.condition.icon}`
+                console.log(dataAllWeek.forecast.forecastday[i].day.avgtemp_c, day)
                 break;
 
             case 3:
-                document.querySelector(".tempWensday").innerHTML = dataAllWeek.forecast.forecastday[3].day.avgtemp_c;
-                weatherIconsWeek[3].src = `${dataAllWeek.forecast.forecastday[3].day.condition.icon}`
-                console.log(dataAllWeek.forecast.forecastday[3].day.avgtemp_c, day)
-                console.log(dataAllWeek.forecast.forecastday[3].day.avgtemp_c, day)
+                document.querySelector(".tempWensday").innerHTML = dataAllWeek.forecast.forecastday[i].day.avgtemp_c;
+                weatherIconsWeek[3].src = `${dataAllWeek.forecast.forecastday[i].day.condition.icon}`
+                console.log(dataAllWeek.forecast.forecastday[i].day.avgtemp_c, day)
+                
                 break;
 
             case 4:
 
-                document.querySelector(".tempThursday").innerHTML = dataAllWeek.forecast.forecastday[4].day.avgtemp_c;
-                weatherIconsWeek[4].src = `${dataAllWeek.forecast.forecastday[4].day.condition.icon}`
-                console.log(dataAllWeek.forecast.forecastday[4].day.avgtemp_c, day)
-                console.log(dataAllWeek.forecast.forecastday[4].day.avgtemp_c, day)
+                document.querySelector(".tempThursday").innerHTML = dataAllWeek.forecast.forecastday[i].day.avgtemp_c;
+                weatherIconsWeek[4].src = `${dataAllWeek.forecast.forecastday[i].day.condition.icon}`
+                console.log(dataAllWeek.forecast.forecastday[i].day.avgtemp_c, day)
+                
                 break;
             case 5:
 
-                document.querySelector(".tempFriday").innerHTML = dataAllWeek.forecast.forecastday[5].day.avgtemp_c;
-                weatherIconsWeek[5].src = `${dataAllWeek.forecast.forecastday[5].day.condition.icon}`
-                console.log(dataAllWeek.forecast.forecastday[5].day.avgtemp_c, day)
-                console.log(dataAllWeek.forecast.forecastday[5].day.avgtemp_c, day)
+                document.querySelector(".tempFriday").innerHTML = dataAllWeek.forecast.forecastday[i].day.avgtemp_c;
+                weatherIconsWeek[5].src = `${dataAllWeek.forecast.forecastday[i].day.condition.icon}`
+                console.log(dataAllWeek.forecast.forecastday[i].day.avgtemp_c, day)
                 break;
 
             case 6:
-                document.querySelector(".tempSaturday").innerHTML = dataAllWeek.forecast.forecastday[6].day.avgtemp_c;
-                weatherIconsWeek[6].src = `${dataAllWeek.forecast.forecastday[6].day.condition.icon}`
-                console.log(dataAllWeek.forecast.forecastday[6].day.avgtemp_c, day)
-                console.log(dataAllWeek.forecast.forecastday[6].day.avgtemp_c, day)
+                document.querySelector(".tempSaturday").innerHTML = dataAllWeek.forecast.forecastday[i].day.avgtemp_c;
+                weatherIconsWeek[6].src = `${dataAllWeek.forecast.forecastday[i].day.condition.icon}`
+                console.log(dataAllWeek.forecast.forecastday[i].day.avgtemp_c, day)
                 break;
             default:
                 break;
